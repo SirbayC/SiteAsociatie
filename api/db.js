b6a11e375e6a4748868a712a152bc2bc330f22d1
Cosmin Andrei Vasilescu <c.vasilescu-1@student.tudelft.nl>
2023-09-09 17:01:10 +0200

    major changes

--- api/db.js
+++ api/db.js
@@ -1,13 +1,11 @@
-import mysql from "mysql"
+import postgres from "postgres"
 
-export const db = mysql.createConnection({
-    host:"localhost",
-    user:'root',
-    password:'123456789Minu',
-    database:'asocdatabase'
-})
+export const db = postgres({
+    host                 : 'ec2-52-215-68-14.eu-west-1.compute.amazonaws.com',            // Postgres ip address[s] or domain name[s]
+    port                 : '5432',          // Postgres server port[s]
+    database             : 'd9sc3hn05baohk',            // Name of database to connect to
+    username             : 'xlvdmgazrwmznz',            // Username of database user
+    password             : 'da4cfcf5639a8b220d122979a643408d0dd4108c0193ee020cc7e8e35f722c75',   
+    ssl : 'require'         // Password of database user
+  })
 
-db.connect(err => {
-    if(err) console.log(err)
-    else console.log("Connected to database")
-})
--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import express from "express"
 import postRoutes from "./routes/postRoute.js"
 import cors from "cors"
 
+const PORT = 8800
+
 const app = express()
 
 app.use(express.json())
@@ -12,7 +14,11 @@ app.use(cors())
 // app.use("/api/auth", authRoutes)
 app.use("/api/posts", postRoutes)
 
+app.get('/', (req, res) => {
+    res.json("Yess")
+  });
+  
 
-app.listen(8800,()=>{
-    console.log("Api Running!")
+app.listen(process.env.PORT || PORT,()=>{
+    console.log(`Api Running o ${PORT}!`)
 })
