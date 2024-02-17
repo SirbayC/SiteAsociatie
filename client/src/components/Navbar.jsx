7e7dc96b65f6aaf0c9ccb31c1437fa75408b7d2e
Kocmnh <cosmin1vasilescu@yahoo.com>
2024-02-17 13:40:28 +0100

    v1 navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Logo from "../resources/logo_transparent.png"
 import User from "../resources/user.png"
 import { NavLink } from 'react-router-dom'
@@ -12,49 +12,19 @@ const Navbar = () => {
     // };
 
     const [showNavbar, setShowNavbar] = React.useState(false);
-
+    const inputElement = React.useRef();
+    
     const handleShowNavbar = () => {
-        setShowNavbar(!showNavbar);
+      setShowNavbar(!showNavbar);
     };
-
-
+    
     const Hamburger = () => (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="52"
-            height="24"
-            viewBox="0 0 52 24"
-        >
-            <g id="Group_9" data-name="Group 9" transform="translate(-294 -47)">
-                <rect
-                    id="Rectangle_3"
-                    data-name="Rectangle 3"
-                    width="42"
-                    height="4"
-                    rx="2"
-                    transform="translate(304 47)"
-                    fill="#574c4c"
-                />
-                <rect
-                    id="Rectangle_5"
-                    data-name="Rectangle 5"
-                    width="42"
-                    height="4"
-                    rx="2"
-                    transform="translate(304 67)"
-                    fill="#574c4c"
-                />
-                <rect
-                    id="Rectangle_4"
-                    data-name="Rectangle 4"
-                    width="52"
-                    height="4"
-                    rx="2"
-                    transform="translate(294 57)"
-                    fill="#574c4c"
-                />
-            </g>
-        </svg>
+      <label htmlFor="check" >
+            <input type="checkbox" id="check" checked={showNavbar} />
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
     );
 
 
@@ -71,19 +41,19 @@ const Navbar = () => {
                         <Hamburger />
                     </div>
                     <div className={`links ${showNavbar && "showMenu"}`}>
-                        <NavLink to="/" className={({ isActive }) => isActive ? "link active" : 'link'}>
+                        <NavLink to="/" className={`link ${({ isActive }) => isActive ? "active" : ''} ${ showNavbar ? "visibleLink" : 'invisibleLink'}`} onClick={handleShowNavbar}>
                             Cunoaste-ne
                         </NavLink>
-                        <NavLink to="/da_pot" className={({ isActive }) => isActive ? "link active" : 'link'}>
+                        <NavLink to="/da_pot" className={`link ${showNavbar && "visibleLink"} ${ showNavbar ? "visibleLink" : 'invisibleLink'}`} onClick={handleShowNavbar}>
                             Da, pot!
                         </NavLink>
-                        <NavLink to="/scrisori" className={({ isActive }) => isActive ? "link active" : 'link'}>
+                        <NavLink to="/scrisori" className={`link ${showNavbar && "visibleLink"} ${ showNavbar ? "visibleLink" : 'invisibleLink'}`} onClick={handleShowNavbar}>
                             Scrisori catre Prieteni
                         </NavLink>
-                        <NavLink to="/lumea_imagini" className={({ isActive }) => isActive ? "link active" : 'link'}>
+                        <NavLink to="/lumea_imagini" className={`link ${({ isActive }) => isActive ? "active" : ''} ${ showNavbar ? "visibleLink" : 'invisibleLink'}`} onClick={handleShowNavbar}>
                             Lumea mea in imagini
                         </NavLink>
-                        <NavLink to="/implica_te" className={({ isActive }) => isActive ? "link active" : 'link'}>
+                        <NavLink to="/implica_te" className={`link ${({ isActive }) => isActive ? "active" : ''} ${ showNavbar ? "visibleLink" : 'invisibleLink'}`} onClick={handleShowNavbar}>
                             Implica-te gratuit
                         </NavLink>
                         {/* <div className="userContainer" onMouseEnter={toggleSubmenu} onMouseLeave={toggleSubmenu}>
