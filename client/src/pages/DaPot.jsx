e974f1cd18b3e644883a02997a29f40f8c3327a9
Kocmnh <cosmin1vasilescu@yahoo.com>
2024-02-20 11:01:47 +0100

    Update PDF source URL in DaPot.jsx

--- client/src/pages/DaPot.jsx
+++ client/src/pages/DaPot.jsx
@@ -21,7 +21,7 @@ const DaPot = () => {
             isLoading ? <LoadingSpinner /> : null
           } 
           <iframe
-            src={pdf}
+            src='https://drive.google.com/file/d/1AdxNm1A0rL9Ruiot33OIm5BntVJMRuSp/preview'
             title="brosura"
             width="100%"
             height="600"
