213b23994c5dc3c032a8b4da2726faa70f306b29
Kocmnh <cosmin1vasilescu@yahoo.com>
2024-03-12 20:25:38 +0100

    add album + fix navbar z index

---
+++ client/src/resources/Album2023.jsx
@@ -0,0 +1,24 @@
+const Album2023 = () => {
+
+    return (
+        <div className="pa-gallery-player-widget"
+            data-link="https://photos.app.goo.gl/foFiqU3HTxgVHarEA"
+            data-title=""
+            data-description="poze 2023">
+            <object data="https://lh3.googleusercontent.com/pw/AP1GczOSLdk46RtlxhlRClrZAy21Zz6Q7nw2ijV3uvE56oYUpTRvUBQlE5DYUHZlahcED9r7jlVhBaCs3vbMjFVE8x7zCksep4CUFhqlC-AYBCJnvBk59MzK=w1920-h1080"></object>
+            <object data="https://lh3.googleusercontent.com/pw/AP1GczOzQn08JbfoxuCfLSHNq8QYoewUlzkGuJwhTyDWcDZc-Hz-SpbzWlJjirdGU3zrb-Ca70VyPTGWcI3d_CVenhURjIW7zlt0SwNWEgF6gWGHqszP8gme=w1920-h1080"></object>
+            <object data="https://lh3.googleusercontent.com/pw/AP1GczOzdM_mhxG-g1USKm9S8dEXTF0PINlZQQULP4pHrrqeeLCMDUv18XisYSEYDupyf1GvVcQMZNVwZHXgB-8_z8l-FbcSWSRnB9Gzjc6lGqS59ZBCJpDZ=w1920-h1080"></object>
+            <object data="https://lh3.googleusercontent.com/pw/AP1GczN3cJkBOys9xjFs_RDEVFHw1Y88QG6dBQeOhTQmoD7ky6eNKIlGHcf1Iy7vZzh-N840Z4y31ST_qB9nk9RzGCdZzGrnmuS-XlZZSu_NwjASTfuq1xHZ=w1920-h1080"></object>
+            <object data="https://lh3.googleusercontent.com/pw/AP1GczP32I96tqU_-xzSmTUPD01XRJLRsZoxIY3gYFdWZiRhJqNe6_Gz2nmEI8MM4vkbwTUIIJUxe8-y7QB6_UvnOlAyzpBV1p9S4392CqKR4J9fgKJInb0l=w1920-h1080"></object>
+            <object data="https://lh3.googleusercontent.com/pw/AP1GczOvWp1h3eK68hJLKJeaXeJvAfxP6HO44_2JIFdm5Ck8ki66JR-OtSLEc1o5_1iXkCQt7viz5QS-idIjc0jbRdbyrSTBitYz8nS_d1Dv76jniCJl72XT=w1920-h1080"></object>
+            <object data="https://lh3.googleusercontent.com/pw/AP1GczNwmOplEz_wFEIXpeLpj3ZyA1tKDDkobsodi0XYObjsm2thrcMIIhJ-LsoOZCCPyrvQhZNFXqGKDBBrgMYw42emzb4XhixLNFMLummUsz7uGstL0Y0E=w1920-h1080"></object>
+            <object data="https://lh3.googleusercontent.com/pw/AP1GczOI8qjGoIb9SwO0zlGPUF7yS3-4kNPMAGgI2KgT7x65x1fGwtXX4SdT2Iy63-jnMsFfOF-3OXBemohOTvZ05hXviGl-1g94pvhPo9VI2-zC-CStHVNr=w1920-h1080"></object>
+            <object data="https://lh3.googleusercontent.com/pw/AP1GczNGvLbQ1o3MWGFj0FDQ-Z-9pNOuMF4UEV8X4KH69andvASxm9zjHgRs1_U-dp3Z4KJ7l0LuQM-il2_ADCFX2RrRDD-V50zXk1rHy1Gn4x7VbL_tB6Tg=w1920-h1080"></object>
+            <object data="https://lh3.googleusercontent.com/pw/AP1GczPjp7Ye9crRemevZS_NC_SbV51RNWyVwY5eRrAVOYp2ekWJp2Iyl4uMdR5GnlyVtFFYfoJjAk_T5-5bPaPN4iuPVj2qd9Ko9PFy6D15iUeMVjtc6ZoK=w1920-h1080"></object>
+            <object data="https://lh3.googleusercontent.com/pw/AP1GczOtpks2pV5ixnIN6hcK1MV9xfP5oCxQPjbEz-RVKwl2-E3THvxneBzxW5jWUBunAARWwReWcRp6iHk73US5XQf19Njf1BUinAYqGWHrDz6od4bZXJTK=w1920-h1080"></object>
+            <object data="https://lh3.googleusercontent.com/pw/AP1GczNENz0oRiEUpJS0FUXQ_xVRoTcvhnUZv9do2s3GIeW-ksDEKjnuI83Gkuk0X2u4A7O_tEtNNyz3NaoRhUlQ_YfkONvCabfTzVRV4G21BeVT2ttMojYE=w1920-h1080"></object>
+        </div>
+    );
+}
+
+export default Album2023;
